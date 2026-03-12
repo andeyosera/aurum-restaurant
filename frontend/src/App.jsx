8bcef743add8d1e756ea21eeb39952f00d3244d2
andeyosera <andeyoseraphina@gmail.com>
2026-03-12 14:01:53 +0300

    test: add home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 export default function App() {
-  return <h1 style={{ color: 'white' }}>Test</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
