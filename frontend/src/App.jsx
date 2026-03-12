1841c85a5d9802d3f8b733341caf0bc4b3b048cb
andeyosera <andeyoseraphina@gmail.com>
2026-03-12 13:56:45 +0300

    test: simplify app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,3 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Menu from './pages/Menu'
-import Booking from './pages/Booking'
-import Contact from './pages/Contact'
-
 export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </>
-  )
+  return <h1 style={{ color: 'white' }}>Test</h1>
 }
